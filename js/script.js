e519761dc58b573b715a014dcd2a53f1ca514bbe
jangelroa <jangelroa@gmail.com>
2014-09-22 22:57:18 -0700

    final product

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+// APPLY FUNCTIONALITY TO THE CENTRAL BUTTON 
+$("#insider a").on("click", function(){
+	// CHANGE THE TEXT IN THE PARAGHAPH ABOVE THE BUTTON
+	$("#insider h1").text("This is  SUPER SUPER SUPER TERRICFIC!");
+});
